f520457e7f410d91b2443a6baf966446c7c6a4c2
johnrbryant <john.r.bryant@gmail.com>
2018-07-29 10:33:46 +1200

    reordered county names in Sweden data

--- data-raw/sweden_births/sweden_births.R
+++ data-raw/sweden_births/sweden_births.R
@@ -5,26 +5,27 @@ library(dplyr)
 library(readr)
 
 county_names <- c(Stockholm = "Stockholm",
-                  Uppsala = "Uppsala",
-                  Sodermanland = "S\xfc\xbe\x8d\xa6\x98\xbcdermanland",
+                  Gotaland = "V\xfc\xbe\x8d\x86\x94\xbcstra G\xfc\xbe\x8d\xa6\x98\xbctaland",
+                  Skane = "Sk\xfc\xbe\x8d\x96\x94\xbcne",
                   Ostergotland = "\xfc\xbe\x8d\xa6\x90\xbcsterg\xfc\xbe\x8d\xa6\x98\xbctland",
                   Jonkoping = "J\xfc\xbe\x8d\xa6\x98\xbcnk\xfc\xbe\x8d\xa6\x98\xbcping",
-                  Kronoberg = "Kronoberg",
-                  Kalmar = "Kalmar",
-                  Gotland = "Gotland",
-                  Blekinge = "Blekinge",
-                  Skane = "Sk\xfc\xbe\x8d\x96\x94\xbcne",
-                  Halland = "Halland",
-                  Gotaland = "V\xfc\xbe\x8d\x86\x94\xbcstra G\xfc\xbe\x8d\xa6\x98\xbctaland",
-                  Varmland = "V\xfc\xbe\x8d\x86\x94\xbcrmland",
-                  Orebro = "\xfc\xbe\x8d\xa6\x90\xbcrebro",               
-                  Vastmanland = "V\xfc\xbe\x8d\x86\x94\xbcstmanland",
-                  Dalarna = "Dalarna",                       
+                  Uppsala = "Uppsala",
+                  Sodermanland = "S\xfc\xbe\x8d\xa6\x98\xbcdermanland",
+                  Orebro = "\xfc\xbe\x8d\xa6\x90\xbcrebro",
+                  Norrbotten = "Norrbotten",
                   Gavleborg = "G\xfc\xbe\x8d\x86\x94\xbcvleborg",
-                  Vasternorrland = "V\xfc\xbe\x8d\x86\x94\xbcsternorrland",
-                  Jamtland = "J\xfc\xbe\x8d\x86\x94\xbcmtland",
+                  Dalarna = "Dalarna",                       
+                  Vastmanland = "V\xfc\xbe\x8d\x86\x94\xbcstmanland",
+                  Kalmar = "Kalmar",
                   Vasterbotten = "V\xfc\xbe\x8d\x86\x94\xbcsterbotten",
-                  Norrbotten = "Norrbotten")
+                  Halland = "Halland",
+                  Vasternorrland = "V\xfc\xbe\x8d\x86\x94\xbcsternorrland",
+                  Varmland = "V\xfc\xbe\x8d\x86\x94\xbcrmland",
+                  Kronoberg = "Kronoberg",
+                  Blekinge = "Blekinge",
+                  Jamtland = "J\xfc\xbe\x8d\x86\x94\xbcmtland",
+                  Gotland = "Gotland")
+
 
 sweden_births <- read_csv("data-raw/sweden_births/BE0101E2.csv",
                           skip = 2) %>%
@@ -32,7 +33,8 @@ sweden_births <- read_csv("data-raw/sweden_births/BE0101E2.csv",
     gather(key = time, value = count, -county, convert = TRUE) %>%
     mutate(county = sub("^[0-9][0-9]\\s", "", county),
            county = sub(" county$", "", county),
-           county = names(county_names)[match(county, county_names)]) %>%
+           county = names(county_names)[match(county, county_names)],
+           county = factor(county, levels = names(county_names))) %>%
     filter(time %in% 1995:2015) %>%     # to match years in book
     xtabs(count ~ county + time, data = .) %>%
     array(dim = dim(.), dimnames = dimnames(.))
