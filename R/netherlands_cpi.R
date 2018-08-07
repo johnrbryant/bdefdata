24fefcb63a3c27a4ec3829d6e2778dff0f6a21b3
johnrbryant <john.r.bryant@gmail.com>
2018-08-07 20:11:52 +1200

    corrected error in netherlands_cpi

--- R/netherlands_cpi.R
+++ R/netherlands_cpi.R
@@ -1,9 +1,10 @@
 
-#' Consumer price index in the Netherlands, 1996-2015
+#' Consumer price index in the Netherlands, 2003-2011
 #' 
 #' @format An array with dimension 'time'.
 #'
-#' @source Table "Key Short-Term Economic Indicators: Consumer
+#' @source Calculated from data in table
+#' "Key Short-Term Economic Indicators: Consumer
 #' Prices - Annual inflation"from OECD database 'OECD.Stat'.
 #' Data downloaded 3 June 2016.
 "netherlands_cpi"
--- data-raw/netherlands_cpi/netherlands_cpi.R
+++ data-raw/netherlands_cpi/netherlands_cpi.R
@@ -7,6 +7,8 @@ library(readr)
 netherlands_cpi <- read_csv("data-raw/netherlands_cpi/KEI_04062016052113516.csv") %>%
     filter(Frequency == "Annual") %>%
     select(time = Time, value = Value) %>%
+    filter(time %in% 2003:2011) %>%
+    mutate(value = value / last(value)) %>%
     xtabs(value ~ time, data = .) %>%
     array(., dim = dim(.), dimnames = dimnames(.))
 
